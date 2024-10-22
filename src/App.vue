ad35c06624426ae00104521bac4b0a22343662f1
pr23 <pr23jat@gmail.com>
2024-10-22 11:04:12 +0700

    fix: navigation layout on android

--- src/App.vue
+++ src/App.vue
@@ -24,18 +24,23 @@ const handleBack = () => {
       break
   }
 }
+
+const currentRouteName = computed(() => {
+  return router.currentRoute.value.name ?? HOME_ROUTE
+})
+
 </script>
 
 <template>
   <div class="relative min-h-[100dvh] min-w-[100dvw] bg-[#F6F7F9]">
     <div class="w-full py-5 text-center bg-white shadow-inset relative">
-      <div v-if="router.currentRoute.value.name !== HOME_ROUTE" class="absolute left-5 top-5 text-blue font-sm text-[30px] leading-[26px]" @click="handleBack()"><</div>
-      <h1 class="text-blue w-full font-medium text-[22px] leading-[26px]">{{ router.currentRoute.value.name }}</h1>
+      <div v-if="currentRouteName !== HOME_ROUTE" class="absolute left-5 top-5 text-blue font-sm text-[30px] leading-[26px]" @click="handleBack()"><</div>
+      <h1 class="text-blue w-full font-medium text-[22px] leading-[26px]">{{ currentRouteName }}</h1>
     </div>
 
     <main class="h-full w-full">
       <router-view :is-rendered="routerStore.router.isVisited" />
-      <WelcomePage :is-rendered="!routerStore.router.isVisited" />
+      <WelcomePage :is-rendered="currentRouteName === HOME_ROUTE" />
     </main>
   </div>
 </template>
